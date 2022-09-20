8ef278bc4505457486f748c5990a81f55c7bda73
Antonio Henriques <antoniohenriques52@gmail.com>
2022-09-20 10:34:15 -0300

    commit_Erudio14

---
+++ GeekShopping.CartAPI/Model/Context/SqlContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace GeekShopping.CartAPI.Model.Context
+{
+    public class SqlContext : DbContext
+    {
+        public SqlContext() {}
+        public SqlContext(DbContextOptions<SqlContext> options) : base(options) {}
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<CartDetail> CartDetails { get; set; }
+        public DbSet<CartHeader> CartHeaders { get; set; }
+
+       
+    }
+}
